4f5e57ae674c6f215ba8fd4c20fc84b47c6c0bc8
Muhammad Ahmed <ahmed@logicielservice.com>
2022-04-08 06:54:10 -0400

    added vim plug install to nvim script

--- install-nvim.sh
+++ install-nvim.sh
@@ -11,3 +11,6 @@ chmod u+x nvim.appimage
 
 sudo mv squashfs-root /
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
