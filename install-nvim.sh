bdb71a77128a612b296e56fdf51798ef0c416c63
Muhammad Ahmed <ahmed@logicielservice.com>
2022-04-08 07:00:30 -0400

    added clang for clangd in nvim script

--- install-nvim.sh
+++ install-nvim.sh
@@ -14,3 +14,5 @@ sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+sudo dnf install clang clang-devel -y
